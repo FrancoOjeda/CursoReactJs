9791a9bf2c1b1ad14370281ecd9954e3ffa4cd94
Franco_Ojeda <24martin94@gmail.com>
2024-01-04 10:57:37 -0300

    Primera Clase

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,36 @@
+import "./assets/App.css";
+import { TwitterFollowCard } from "./assets/TwitterFollowCard";
+const users = [
+  {
+    userName: "midudev",
+    name: "Miguel Angel Duran",
+    isFollowing: false,
+  },
+  {
+    userName: "pheralb",
+    name: "Pablo Hernandez",
+    isFollowing: true,
+  },
+  {
+    userName: "Elon Musk",
+    name: "Elon Musk",
+    isFollowing: false,
+  },
+];
+export function App() {
+  return (
+    <section className="App">
+      {users.map((user) => {
+        const { userName, name, isFollowing } = user;
+        return (
+          <TwitterFollowCard
+            name={name}
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          />
+        );
+      })}
+    </section>
+  );
+}
