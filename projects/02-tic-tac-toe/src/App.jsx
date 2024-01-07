653a7f0f8b94813a6a8b246c2e0207266be71b51
Franco_Ojeda <24martin94@gmail.com>
2024-01-07 11:06:19 -0300

    Agregado de eslint y localStorage

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -5,15 +5,27 @@ import { Square } from "./components/Square.jsx";
 import { WinnerModal } from "./components/WinnerModal.jsx";
 import { TURNS } from "./constants";
 import { checkWinnerFrom, checkEndGame } from "./logic/board";
+import { saveGameStorage, resetGameStorage } from "./logic/storage/index.js";
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromLocalStorage = window.localStorage.getItem("board");
+    return boardFromLocalStorage
+      ? JSON.parse(boardFromLocalStorage)
+      : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromLocalStorage = window.localStorage.getItem("turn");
+    return turnFromLocalStorage ?? TURNS.X;
+    // El doble signo de pregunta en la ternaria verifica si la primera condicion es null o undifine
+  });
   const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    resetGameStorage();
   };
   const updateBoard = (index) => {
     if (board[index] || winner) return;
@@ -22,6 +34,8 @@ function App() {
     setBoard(newBoard);
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    // Guardar partida en Local Storage
+    saveGameStorage({ board: newBoard, turn: newTurn });
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
       confetti();
